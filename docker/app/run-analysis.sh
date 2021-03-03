5f50ff9b7d49d2607413d82622823708eec8fba5
drtsb <ruthlessbastard@gmail.com>
2021-03-04 00:56:27 +0500

    deptrac fix

--- docker/app/run-analysis.sh
+++ docker/app/run-analysis.sh
@@ -7,6 +7,8 @@ echo "Running Code Style Analysis..."
 echo "Running PHP Static Analysis..."
 php -d memory_limit=-1 /app/vendor/bin/phpstan analyse -c /app/phpstan.neon.dist
 
-echo "Running deptrac..."
+echo "Running deptrac for modules..."
 /app/vendor/bin/deptrac analyse depfile-modules.yaml
+
+echo "Running deptrac for layers..."
 /app/vendor/bin/deptrac analyse depfile-layers.yaml
