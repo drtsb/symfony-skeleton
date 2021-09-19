53c06e3e168f2bfbb6882b24d9e2b2ccd426bd5c
drtsb <ruthlessbastard@gmail.com>
2021-09-19 17:43:37 +0500

    fix doctrine config

--- config/packages/doctrine.php
+++ config/packages/doctrine.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    $containerConfigurator->import(__DIR__ . '/doctrine/*');
+    $containerConfigurator->import(__DIR__ . '/doctrine/');
 
     $containerConfigurator->extension(
         'doctrine',
